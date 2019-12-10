067dce3448578b5168e7e818f2259021f215cca1
20181106279 <1771478860@qq.com>
2019-12-10 16:56:04 +0800

    Initial Commit

---
+++ arduino4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  arduino4
+//
+//  Created by 20181106279 on 2019/12/10.
+//  Copyright © 2019 20181106279. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
